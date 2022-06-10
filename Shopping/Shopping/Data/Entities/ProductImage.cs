5e0c346298f7711665304b8c75e4123c350e5c26
Wilson Loayza <wilson_loayza@hotmail.com>
2022-06-10 22:20:04 +0200

    Add Product

---
+++ Shopping/Shopping/Data/Entities/ProductImage.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class ProductImage
+    {
+        public int Id { get; set; }
+
+        public Product Product { get; set; }
+
+        [Display(Name = "Foto")]
+        public Guid ImageId { get; set; }
+
+        [Display(Name = "Foto")]
+        public string ImageFullPath => ImageId == Guid.Empty
+            ? $"https://localhost:7090/images/noimage.png"
+            : $"https://shoppingwill.blob.core.windows.net/products/{ImageId}";
+
+    }
+}
